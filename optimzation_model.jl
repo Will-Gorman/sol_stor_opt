85282bbbd1b55463254dfe261c2fdce4c3dcbaa2
Will Gorman <will.gorman5@gmail.com>
2018-04-13 08:05:52 -0700

    Updated to version 2 of model

--- optimzation_model.jl
+++ optimzation_model.jl
@@ -1,6 +1,7 @@
-using CSV, JuMP, Ipopt, ConditionalJuMP
+using CSV, JuMP, Gurobi #CPLEX
 
 # Define Constants
+#DIR = "C:\\Users\\will-\\Google Drive\\UCBerkeley\\2018Spring\\Comp Programing in Econ\\Project\\"
 DIR = "C:\\Users\\Will\\GoogleDrive\\UCBerkeley\\2018Spring\\Comp Programing in Econ\\Project\\"
 OUT = "out"
 INPUT = "in"
@@ -9,7 +10,7 @@ BAT_RATE = 0.12
 BAT_EFF = 0.92
 PV_COST = 1500
 PV_RATE = 0.07
-LOAD_SHED = 0.01
+LOAD_SHED = 0.0000001
 NEM = 0.12
 
 # Load in solar and load data
@@ -18,45 +19,34 @@ load = CSV.read(DIR * INPUT * "\\BASE\\690150.csv")
 load = load[:,2]
 tot_load = sum(load)
 
-## write conditional functions
-function input(x)
-    @ifelse(x < 0, 
-      0,
-      x
-      )
-end
-
-function solar(x)
-    @ifelse(x == 0, 
-      0,
-      x
-      )
-end
-
 # Set-up optimization model
-m = Model(solver = IpoptSolver()) # specify the solver, pass along to model
+m = Model(solver = GurobiSolver()) # specify the solver, pass along to model 	CplexSolver() GurobiSolver()
 
 @variables m begin
-    x >= 0 ## solar capacity
-    y >= 0 ## storage capacity
-    y >= bat_chg[0:8760] >= 0. ## battery state of charge
-    ex_load[1:8760] >= 0. ## excess load pre-battery
-    ex_pv[1:8760] >= 0. ## excess pv pre-battery
-    shed[1:8760] >= 0. ## total load shed with battery
-    pv_rpf[1:8760] >= 0.  ## total extra pv with battery
+    x >= 0. ## solar capacity
+    y >= 0. ## storage capacity
+    #state[i=1:8760], Int ## if discharging or charging
+    power_in[i=1:8760] >= 0. ## charging amount
+    power_out[i=1:8760] >= 0. ## discharging amount
+    bat_chg[i=0:8760] >= 0. ## battery state of charge
+    shed[i=1:8760] >= 0. ## total load shed with battery
+    pv_rpf[i=1:8760] >= 0.  ## total extra pv with battery
 end
 
 @constraints(m, begin
-    bat_chg[0] == y
-    loading[i=1:8760], ex_load[i] == input((load[i] - sol[i] * x)) ## if these are defined outside of the model, will it work? 
-    solar[i=1:8760], ex_pv[i] == input((sol[i] * x - load[i]))
-    reverse[i=1:8760], pv_rpf[i] == solar(ex_pv[i] - (bat_chg[i] - bat_chg[i-1]) / BAT_EFF)
-    charging[i=1:8760], bat_chg[i] == bat_chg[i-1] + ex_pv[i] * y - ex_load[i]
-    shed_load[i=1:8760], shed[i] == bat_chg[i] - bat_chg[i-1] - ex_load[i]
-    sum(shed[i] for i:8760) <= tot_load * LOAD_SHED
+    charging[0], bat_chg[0] == y / 2
+    charging2[i=1:8760], bat_chg[i] == bat_chg[i-1] + power_in[i] - power_out[i]
+    charging3[i=1:8760], bat_chg[i] <= y 
+    balance[i=1:8760], load[i] + power_in[i] + pv_rpf[i] == power_out[i] + (sol[i] * x) + shed[i] 
+    powering[i=1:8760], power_in[i] <= y #* state[i]
+    depowering[i=1:8760], power_out[i] <= y #* (1-state[i])
+    bat_power1[i=1:8760], power_in[i] + bat_chg[i] <= y
+    bat_power2[i=1:8760], bat_chg[i] - power_out[i] >= 0 
+    sum(shed[i] for i in 1:8760) <= tot_load * LOAD_SHED
 end)
 
-@objective(m, Min, x * PV_COST * PV_RATE + y * BAT_COST * BAT_RATE + (sum(shed[i] for i:8760) - sum(pv_rpf[i] for i:8760)) * NEM) 
+@objective(m, Min, x * PV_COST * PV_RATE + y * BAT_COST * BAT_RATE + (sum(shed[i] for i in 1:8760) * NEM)) 
 
 solve(m)
 getvalue(x)
+getvalue(y)
